5628c70139905eb9e6b26e969126b66c902f4fde
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-07 06:50:59 +0100

    hello express

--- week01/003_hello_express.js
+++ week01/003_hello_express.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// handle base route
 app.get('/', (req, res) => {
 	res.send("Hello Express");
 })
 
-app.listen(port);
-console.log(`serving at http://localhost:${port}`);
+// handle any other request
+app.use((req, res) => {
+	res.status(404).send('404 - Nothing to be seen here');
+});
+
+// start the server
+const server = app.listen(port, () => {
+	console.log(`serving at http://localhost:${server.address().port}`);	
+});
+
